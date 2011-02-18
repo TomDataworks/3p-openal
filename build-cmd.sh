1d131f2d5f76519ef2b4fd2bbbbf615dd239a9df
Andrew A. de Laix <alain@lindenlab.com>
2011-02-18 11:15:47 -0800

    shell build script.

---
+++ build-cmd.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# turn on verbose debugging output for parabuild logs.
+set -x
+# make errors fatal
+set -e
+
+OPENAL_VERSION="1.12.854"
+OPENAL_SOURCE_DIR="openal-soft-$OPENAL_VERSION"
+
+if [ -z "$AUTOBUILD" ] ; then 
+    fail
+fi
+
+if [ "$OSTYPE" = "cygwin" ] ; then
+    export AUTOBUILD="$(cygpath -u $AUTOBUILD)"
+fi
+
+# load autbuild provided shell functions and variables
+set +x
+eval "$("$AUTOBUILD" source_environment)"
+set -x
+
+stage="$(pwd)"
+case "$AUTOBUILD_PLATFORM" in
+    "windows")
+    ;;
+    "darwin")
+    ;;
+    "linux")
+    ;;
+esac
+mkdir -p "$stage/LICENSES"
+cp COPYING > "$stage/LICENSES/openal.txt"
+
+pass
+
